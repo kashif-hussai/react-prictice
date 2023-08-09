fc8a9bc34344ff66bc2094523f53d9794cb23681
kashif-hussai <kashifhussain.xwave@gmail.com>
2023-08-09 13:41:21 +0500

    react project commit is converted

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Body from './components/body/Body';
+
+
+function App() {
+  return <>
+
+  <Header/>
+  <Body/>
+  <Footer/>
+  </>;
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = ()=>{
+
+    return(
+        <div>
+            <h1 class="header">welcome to react jungle</h1>
+            
+        </div>
+    )
+}
+export default Header
